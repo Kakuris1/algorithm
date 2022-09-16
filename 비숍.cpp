5cc180a9af6efbb4a8d7a601404463e60c793d7a
Kakuris1 <103870664+Kakuris1@users.noreply.github.com>
2022-09-16 21:02:22 +0900

    [백준 1799번 골드1] 비숍
    # 백트래킹

--- 비숍.cpp
+++ 비숍.cpp
@@ -1,4 +1,60 @@
-//1회차
+//
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N; // 체스판 크기
+int board[10][10];
+bool put_R[19];//좌상단->우하단 : 두 수의 차가 같음
+bool put_L[19];//우상단->좌하단 : 두 수의 합이 같음
+int max_result[2];
+
+void bishop(int x, int y, int bishop_cnt, int color)
+{
+	if (x >= N) { // 다음 줄 이동
+		if (x % 2 == 0) { x = 1; }
+		else if (x % 2 == 1) { x = 0; }
+		y++;
+	}
+	// 탈출조건
+	if (y >= N) {
+		max_result[color] = max(max_result[color], bishop_cnt);
+		return;
+	}
+	// 비숍 못 놓음
+	if (board[y][x] == 0 || put_R[9 + x - y] == true || put_L[x + y] == true) {
+		bishop(x + 2, y, bishop_cnt, color);
+	}
+	else {	// 비숍 놓을 수 있음
+		// 비숍 놓고 다음 시행
+		put_R[9 + x - y] = true;
+		put_L[x + y] = true;
+		bishop(x + 2, y, bishop_cnt + 1, color);
+		// 복구
+		put_R[9 + x - y] = false;
+		put_L[x + y] = false;
+		// 비숍 안놓고 다음 시행
+		bishop(x + 2, y, bishop_cnt, color);
+	}
+}
+
+int main()
+{
+	cin >> N;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			cin >> board[y][x];
+		}
+	}
+	bishop(0, 0, 0, 0);
+	bishop(1, 0, 0, 1);
+	cout << max_result[0] + max_result[1];
+}
+
+
+
+
+/*1회차
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -107,3 +163,4 @@ int main()
 	cout << result_max_bishop;
 }
 //
+*/
