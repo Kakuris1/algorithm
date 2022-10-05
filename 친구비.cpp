c83e377d8546a1c3b0bad7f0a2a9d2449a6d5c85
Kakuris1 <103870664+Kakuris1@users.noreply.github.com>
2022-10-05 14:14:55 +0900

    [백준 16562번 골드4] 친구비
    # 자료구조
    # 분리집합
    # 유니온파인드
    # 우선순위 큐

--- 친구비.cpp
+++ 친구비.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<algorithm>
+#include<queue>
 using namespace std;
 int p[10001];
 int N, M, K;
 int f_cost[10001];
+priority_queue<int, vector<int>, greater<int>>Q[10001]; // 작은것 부터 출력
 
 int find(int n)
 {
@@ -23,9 +26,23 @@ void Union(int a, int b)
 int main()
 {
 	cin >> N >> M >> K;
+	for (int i = 1; i <= N; i++) { p[i] = i; }
 	for (int i = 1; i <= N; i++) { cin >> f_cost[i]; }
 	for (int i = 1; i <= M; i++) {
 		int tmp1, tmp2;
 		cin >> tmp1 >> tmp2;
+		Union(min(tmp1, tmp2), max(tmp1, tmp2));
 	}
+
+	for (int i = 1; i <= N; i++) { 
+		find(i);	// p[i]가 항상 루트노드를 가리키고 있는건 아니므로, find(i)를 한번씩 시행해줌
+		Q[p[i]].push(f_cost[i]);
+	}
+	int sum = 0;
+	for (int i = 1; i <= N; i++) {
+		if (Q[i].empty()) { continue; }
+		sum+=Q[i].top();
+	}
+	if (sum <= K) { cout << sum; }
+	else { cout << "Oh no"; }
 }
