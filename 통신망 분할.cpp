8281f9f5894f18deb944378b99bf5385c5a49d97
Kakuris1 <103870664+Kakuris1@users.noreply.github.com>
2022-12-12 22:27:55 +0900

    Update 통신망 분할.cpp

--- 통신망 분할.cpp
+++ 통신망 분할.cpp
@@ -3,22 +3,50 @@
 #include<queue>
 using namespace std;
 typedef pair<int, int> Pr;
+int N, M, Q;
+Pr Edge[100001];
+int P[100001];
+bool Remove[100001];
+int sequence[100001];
+int cnt_size[100001];
 
-Pr Find(Pr n)
+int Find(int n)
 {
-	if (P[n.second][n.first] == n) { return n; }
-	return P[n.second][n.first] = Find(P[n.second][n.first]);
+	if (P[n] == n) { return n; }
+	return P[n] = Find(P[n]);
 }
-void Union(Pr a, Pr b)
+void Union(Pr n)
 {
-	Pr A = Find(a);
-	Pr B = Find(b);
+	int A = Find(n.first);
+	int B = Find(n.second);
 	if (A == B) { return; }
-	P[B.second][B.first] = A;
+	P[B] = A;
 	return;
 }
 
 int main()
 {
-	
+	fill(cnt_size, cnt_size + 100001, 1); // cnt_size 초기화
+	cin >> N >> M >> Q;
+	for (int i = 1; i <= M; i++){
+		P[i] = i;							// P 초기화
+		int X, Y;
+		cin >> X, Y;
+		Edge[i] = make_pair(X, Y);
+	}
+
+	for (int i = 1; i <= Q; i++) {
+		int tmp;
+		cin >> tmp;
+		sequence[i] = tmp;
+		Remove[tmp] = true;
+	}
+
+	for (int i = 1; i <= M; i++) {
+		if (!Remove[i]) { Union(Edge[i]); }
+	}
+
+	for (int i = Q; i >= 1; i--) {
+
+	}
 }
