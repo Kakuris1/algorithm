1af85461f117795d284551dc9838d3ac18b83f3b
Kakuris1 <103870664+Kakuris1@users.noreply.github.com>
2022-12-13 16:35:43 +0900

    [백준 17398번 골드1] 통신망 분할
    # 자료구조
    # 분리집합
    # 유니온 파인드

--- 통신망 분할.cpp
+++ 통신망 분할.cpp
@@ -9,6 +9,7 @@ int P[100001];
 bool Remove[100001];
 int sequence[100001];
 int cnt_size[100001];
+int Total_cost = 0;
 
 int Find(int n)
 {
@@ -21,17 +22,20 @@ void Union(Pr n)
 	int B = Find(n.second);
 	if (A == B) { return; }
 	P[B] = A;
+	cnt_size[A] += cnt_size[B];
 	return;
 }
 
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
 	fill(cnt_size, cnt_size + 100001, 1); // cnt_size 초기화
 	cin >> N >> M >> Q;
 	for (int i = 1; i <= M; i++){
 		P[i] = i;							// P 초기화
 		int X, Y;
-		cin >> X, Y;
+		cin >> X >> Y;
 		Edge[i] = make_pair(X, Y);
 	}
 
@@ -47,6 +51,15 @@ int main()
 	}
 
 	for (int i = Q; i >= 1; i--) {
-
+		if (Find(Edge[sequence[i]].first) != Find(Edge[sequence[i]].second)) { // 다른 그룹이면
+			Total_cost += cnt_size[Find(Edge[sequence[i]].first)] * cnt_size[Find(Edge[sequence[i]].second)];
+		}
+		Union(Edge[sequence[i]]);
 	}
+
+	cout << Total_cost;
 }
+/* 오답 내역
+*  1. 55번줄 cnt_size[] 놓침
+*  2. Total_cost 자료형 int -> long long 
+*/
